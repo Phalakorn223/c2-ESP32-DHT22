ea83252aa640c82b13aeb105bc0cca90c46b5bcd
Phalakorn223 <phalakorn061@gmail.com>
2022-02-11 22:11:47 +0700

    update

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <DHT.h>
+#include <SPI.h>
+
+DHT mysensor(5, DHT12);
+float temperature, humidity;
+int LED = 4;
+
+void setup()
+{
+  Serial.begin(11250);
+  mysensor.begin();
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+}
+
+void loop()
+{
+  humidity = mysensor.readHumidity();
+  temperature = mysensor.readTemperature();
+  if (temperature >= 33)
+  {
+    digitalWrite(LED,HIGH);
+    Serial.print("LED ON");
+  }else
+  {
+    digitalWrite(LED,LOW);
+    Serial.print("LED OFF");
+  }
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.print("C / Humidity");
+  Serial.print(humidity);
+  Serial.print("%");
+  delay(2000);
+}
